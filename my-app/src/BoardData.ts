1d48948495719ba25232465103e26088d7b40e39
junyeoplee <jyeop920@gmail.com>
2023-06-28 02:41:40 +0900

    [fixed] : 데이터 추가
    데이터 추가해서 검색 필터 기능 작동이 잘되는지 확인

--- my-app/src/BoardData.ts
+++ my-app/src/BoardData.ts
@@ -6,7 +6,7 @@ const PostData:PostListType =
     postList:[
         {
             id:0,
-            title: '첫번째 게시물',
+            title: '충청도 카페',
             author: '작성자1',
             like: 3,
             views: 30,
@@ -14,12 +14,76 @@ const PostData:PostListType =
         },
         {
             id:1,
-            title: '두범째 게시물',
+            title: '제주도 카페',
             author: '작성자2',
             like: 5,
             views: 10,
             comment: 5
-        }
+        },
+        {
+            id:2,
+            title: '부산 광안리',
+            author: '작성자3',
+            like: 2,
+            views: 20,
+            comment: 3
+        },
+        {
+            id:3,
+            title: '부산 해운대',
+            author: '작성자4',
+            like: 5,
+            views: 10,
+            comment: 5
+        },
+        {
+            id:4,
+            title: '괴산 산막이길',
+            author: '작성자5',
+            like: 5,
+            views: 14,
+            comment: 5
+        },
+        {
+            id:5,
+            title: '영종도 바다',
+            author: '작성자6',
+            like: 5,
+            views: 10,
+            comment: 5
+        },
+        {
+            id:6,
+            title: '강화도 바다',
+            author: '작성자7',
+            like: 1,
+            views: 11,
+            comment: 0
+        },
+        {
+            id:7,
+            title: '부천 만화박물관',
+            author: '작성자8',
+            like: 5,
+            views: 18,
+            comment: 8
+        },
+        {
+            id:8,
+            title: '서울 놀이공원',
+            author: '작성자2',
+            like: 5,
+            views: 10,
+            comment: 5
+        },
+        {
+            id:9,
+            title: '서울 카페',
+            author: '작성자2',
+            like: 5,
+            views: 10,
+            comment: 5
+        },
     ]
 }
 
