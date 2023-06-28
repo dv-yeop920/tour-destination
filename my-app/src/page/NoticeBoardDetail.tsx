c3d5dff62fd71c63c9ca9c74042e4696e6ef8509
junyeoplee <jyeop920@gmail.com>
2023-06-29 04:07:16 +0900

    [Add] : CKEditor 이미지 툴 설치
    이미지 업로드를 할 수 있게 하기 위해 설치 해둠

---
+++ my-app/src/page/NoticeBoardDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoticeBoardDetail = () => {
+    return (
+        <>
+            {}
+        </>
+    );
+};
+
+export default NoticeBoardDetail;
--- my-app/src/page/WritingPage.tsx
+++ my-app/src/page/WritingPage.tsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import * as styled from '../style/styledComponents';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
+import { Image } from '@ckeditor/ckeditor5-image';
+import { useNavigate } from 'react-router';
 
 
 
 const WritingPage = () => {
-    const [userWritingValue , setUserWritingValue] = useState('');
+    const [userWritingValue , setUserWritingValue] = useState<string>('');
+    const navigate = useNavigate();
     return (
         <>
         <div className="container mt-4">
@@ -15,20 +19,41 @@ const WritingPage = () => {
         }}>
                 <div className="form-group">
                     <input
+                    id="title"
                     type="text"
                     className="form-control"
                     placeholder='제목'
-                    id="title"/>
+                    onChange={(e) => {
+                        setUserWritingValue(e.target.value);
+                        console.log(userWritingValue)
+                    }}/>
                 </div>
                 <div className="form-group">
                 <CKEditor
                 editor={ClassicEditor}
                 data="<p>행복했던 여행 후기를 공유해 주세요!</p>"
-                />
+                onReady={ editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+                onChange={ ( value, editor ) => {
+                    const data = editor.getData();
+                    setUserWritingValue(data);
+                    console.log(userWritingValue)
+                } }
+                onBlur={ ( event, editor ) => {
+                    console.log( 'Blur.', editor );
+                } }
+                onFocus={ ( event, editor ) => {
+                    console.log( 'Focus.', editor );
+                } }/>
+                
                 </div>
                 <div className='button-group'>
                     <styled.DeleteButton
-                    onClick={() => alert('정말 취소 하시겠습니까?')}
+                    onClick={() => {
+                        navigate('/noticeBoard')
+                    }}
                     type='submit'
                     className='delete-btn'>
                         취소
@@ -36,7 +61,7 @@ const WritingPage = () => {
                     <div style={{width:'82px'}}>
                     </div>
                     <styled.DefaultButton
-                    onClick={() => alert('게시글 등록 하시겠습니까?')}
+                    onClick={() => navigate('/noticeBoard')}
                     type='submit'
                     className='default-btn'>
                         등록
