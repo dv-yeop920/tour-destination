63e972cd8827d7d327e5b2da90f08e3bd4d0ae3f
junyeoplee <jyeop920@gmail.com>
2023-06-28 20:09:07 +0900

    [Refactor] 게시판 코드 리팩토링
    게시글 목록을 보여주던 코드가 너무 길고 지저분해서 PostsRender 함수안에 정리해줌 이때 , posts 라는 변수는 filter된 포스트가 참이면 filterPost를 아니면 기존의 post를 보여준다 라는 내용을 할당 하고 posts에 map을 돌려 상황에 맞게 post들을 보여주도록 리팩토링

--- my-app/src/components/NoticeBoardList.tsx
+++ my-app/src/components/NoticeBoardList.tsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faPenToSquare} from "@fortawesome/free-solid-svg-icons";
 import * as styled from '../style/styledComponents';
 import { useNavigate } from 'react-router';
 import { Post, PostListType } from '../model/Board';
+import WritingIcon from './WritingIcon';
 
 
 
 const NoticeBoardList:React.FC<PostListType> = ({postList}) => {
-    
     const navigate = useNavigate();
     const [userInputValue , setUserInputValue] = useState<string>('');
     const [filteringPost , setFilteringPost] = useState<PostListType>();
+
 
     const handleFilterPost = (userValue:string):void => {
         const filterPost:Post[] = postList.filter((post:Post) => {
@@ -19,111 +18,89 @@ const NoticeBoardList:React.FC<PostListType> = ({postList}) => {
         });
         setFilteringPost({postList:filterPost});
     }
+
+
+    const PostsRender = ():JSX.Element[] => {
+        const posts = filteringPost ? filteringPost.postList : postList;
+
+        return posts.map((post:Post,i:number) => {
+            return(
+                <styled.Li 
+                key={posts[i].id}
+                onClick={() => navigate(`/detail:${post.id}`)
+                }>
+                    <div className='post-box'>
+                        <styled.Title>
+                            {post.title}
+                        </styled.Title>
+
+                        <styled.Author>
+                            {post.author}
+                            <span className='like'>
+                                ❤️
+                            </span>
+                            <span className='like-count'>{post.like}</span>
+                            <span className='watching-count'>조회 {post.views}</span>
+                            <span className='comment-count'>댓글 {post.comment}</span>
+                        </styled.Author>
+                    </div>
+
+                    <div className='post-image-container'>
+                        <img  alt="" 
+                        style={{
+                        width:'80px', 
+                        height:'70px',
+                        marginTop:'4px'}}/>
+                    </div>
+                </styled.Li>
+            );
+        });
+    };
+
     return (
         <>
-        <form onSubmit={(e) => {
+        <form
+        onSubmit={(e) => {
             e.preventDefault();
         }}>
-                <styled.InputBox>
-                    <styled.Input 
-                    type="text"
-                    onChange={(e) => {
-                        setUserInputValue(e.target.value);
-                    }} />
-                    <styled.DefaultButton
-                    onClick={() => {
-                        handleFilterPost(userInputValue);
-                    }}
-                    type='submit'
-                    className='default-btn'>
-                        검색
-                    </styled.DefaultButton>
-                    <FontAwesomeIcon
-                    className ='writing'
-                    onClick={() => {navigate('/writingPage')}}
-                    icon={faPenToSquare}
-                    size = '2x'/>
-                </styled.InputBox>
-            </form>
-            
-            <styled.Board>
-                <styled.Li 
-                onClick={() => navigate('/detail:id')}
-                style={{
-                    height:'100px',
-                    color:'rgb(200, 50, 100)'
+            <styled.InputBox>
+                <styled.Input 
+                type="text"
+                onChange={(e) => {
+                    setUserInputValue(e.target.value);
+                }} />
+                <styled.DefaultButton
+                type='submit'
+                className='default-btn'
+                onClick={() => {
+                    handleFilterPost(userInputValue);
                 }}>
-                    <styled.Title>
-                        [공지]
-                    <styled.Author 
-                    style={{
-                        fontSize:'16px',
-                        color:'black'
-                    }}>
-                        게시판 이용 수칙
-                    </styled.Author>
-                    </styled.Title>
-                </styled.Li>
-                {filteringPost ? filteringPost?.postList.map((post:any , i:any) => {
-                    return(
-                        <styled.Li 
-                        key={postList[i].id}
-                        onClick={() => navigate(`/detail:${post.id}`)}>
-                            <div className='post-box'>
-                                <styled.Title>
-                                    {post.title}
-                                </styled.Title>
-                                <styled.Author>
-                                {post.author}
-                                <span className='like'>
-                                    ❤️
-                                </span>
-                                <span className='like-count'>{post.like}</span>
-                                <span className='watching-count'>조회 {post.views}</span>
-                                <span className='comment-count'>댓글 {post.comment}</span>
-                                </styled.Author>
-                            </div>
-                            <div className='post-image-container'>
-                                <img  alt="" 
-                                style={{
-                                width:'80px', 
-                                height:'70px',
-                                marginTop:'4px'}}/>
-                            </div>
-                        </styled.Li>
-                    )
-                }):
-                postList.map((post , i) => {
-                    return(
-                        
-                        <styled.Li 
-                        key={postList[i].id}
-                        onClick={() => navigate(`/detail:${post.id}`)}>
-                            <div className='post-box'>
-                                <styled.Title>
-                                    {post.title}
-                                </styled.Title>
-                                <styled.Author>
-                                {post.author}
-                                <span className='like'>
-                                    ❤️
-                                </span>
-                                <span className='like-count'>{post.like}</span>
-                                <span className='watching-count'>조회 {post.views}</span>
-                                <span className='comment-count'>댓글 {post.comment}</span>
-                                </styled.Author>
-                            </div>
-                            <div className='post-image-container'>
-                                <img  alt="" 
-                                style={{
-                                width:'80px', 
-                                height:'70px',
-                                marginTop:'4px'}}/>
-                            </div>
-                        </styled.Li>
-                    )
-                })}
-            </styled.Board>
+                    검색
+                </styled.DefaultButton>
+                <WritingIcon/>
+            </styled.InputBox>
+        </form>
+            
+        <styled.Board>
+            <styled.Li 
+            onClick={() => navigate('/detail:id')}
+            style={{
+                height:'100px',
+                color:'rgb(200, 50, 100)'
+            }}>
+                <styled.Title>
+                    [공지]
+                <styled.Author 
+                style={{
+                    fontSize:'16px',
+                    color:'black'
+                }}>
+                    게시판 이용 수칙
+                </styled.Author>
+                </styled.Title>
+            </styled.Li>
+            {PostsRender()}
+        </styled.Board>
         </>
         
     );
