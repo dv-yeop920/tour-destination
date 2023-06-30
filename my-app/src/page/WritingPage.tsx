50d045f34c9c8ff4c65e01632ebaa2e97c65b95b
junyeoplee <jyeop920@gmail.com>
2023-06-30 12:42:33 +0900

    [Remove] : CKEditor 라이브러리 삭제
    사용법이 너무 복잡해 다른 라이브러리를 알아보던가 input을 활용해서 업로드 기능구현 해야할듯 싶음

--- my-app/src/page/WritingPage.tsx
+++ my-app/src/page/WritingPage.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import * as styled from '../style/styledComponents';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
-import { Image } from '@ckeditor/ckeditor5-image';
 import { useNavigate } from 'react-router';
 
 
@@ -27,24 +23,6 @@ const WritingPage = () => {
                         setUserWritingValue(e.target.value);
                         console.log(userWritingValue)
                     }}/>
-                    <CKEditor
-                    editor={ClassicEditor}
-                    data="<p>행복했던 여행 후기를 공유해 주세요!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( value, editor ) => {
-                        const data = editor.getData();
-                        setUserWritingValue(data);
-                        console.log(userWritingValue)
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }/>
                 </div>
 
                 <div className='button-group'>
